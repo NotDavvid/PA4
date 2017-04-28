4a92f76269a4369ab1c5eb0526e0758f322aa13b
NotDavvid <notdavvid@gmail.com>
2017-04-28 18:53:51 -0500

    test2

--- sysfile.c
+++ sysfile.c
@@ -333,30 +333,32 @@ sys_open(void)
 
   begin_op();
 
-  if(omode & O_CREATE){
-    char name[DIRSIZ];
-  ip = create(path, T_FILE, 0, 0);
-  struct inode *dp = nameiparent(path, name);
+  if(!O_SMALLFILE){
+    if(omode & O_CREATE){
+      char name[DIRSIZ];
+    ip = create(path, T_FILE, 0, 0);
+    struct inode *dp = nameiparent(path, name);
 
-  if(ip == 0){
-    end_op();
-    return -1;
-  }
-  if(dp->type == T_SDIR){
-    cprintf("nononon");
-    end_op();
-    return -1;
-  }
-} else {
-  if((ip = namei(path)) == 0){
-    end_op();
-    return -1;
-  }
-  ilock(ip);
-  if(ip->type == T_DIR && omode != O_RDONLY){
-    iunlockput(ip);
-    end_op();
-    return -1;
+    if(ip == 0){
+      end_op();
+      return -1;
+    }
+    if(dp->type == T_SDIR){
+      cprintf("nononon");
+      end_op();
+      return -1;
+    }
+  } else {
+    if((ip = namei(path)) == 0){
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+    if(ip->type == T_DIR && omode != O_RDONLY){
+      iunlockput(ip);
+      end_op();
+      return -1;
+      }
     }
   }
 
