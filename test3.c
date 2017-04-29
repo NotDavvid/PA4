3ce251f1bf058d58b7e9bff80a0c0598600c77d3
NotDavvid <notdavvid@gmail.com>
2017-04-28 20:51:35 -0500

    test2

--- test3.c
+++ test3.c
@@ -2,15 +2,43 @@
 #include "user.h"
 #include "syscall.h"
 #include "stat.h"
+#include "fcntl.h"
 
 
-int
-main(int argc, char *argv[])
+#define N 100
+char buf[8192];
+int stdout = 1;
+
+void
+writetest(void)
 {
-	const uint N = 55748363;
-	printf(1, "malloc test\n");
-	malloc(N);
-	//currentm("test");
-    	exit();
-
+  int rf;
+	char* test = "normdir1";
+	if(mkdir(test) < 0){
+    printf(stdout, "mkdir failed\n");
+    exit();
+  }
+  if(chdir(test) < 0){
+    printf(stdout, "chdir normdir1 failed\n");
+    exit();
+  }
+  /////////////////////////////////////////////////////////////////////////////
+  printf(stdout, "-------------------------\n");
+  printf(stdout, "---Creating Small File---\n");
+  printf(stdout, "---in normal directory---\n");
+  printf(stdout, "-------------------------\n");
+  rf = open("small", O_CREATE|O_SFILE|O_RDWR);
+  if(rf >= 0){
+    printf(stdout, "\tsucceeded\n");
+  } else {
+    printf(stdout, "error: create normal file failed!\n");
+    exit();
+  }
+  /////////////////////////////////////////////////////////////////////////////
+}
+int
+main(void)
+{
+		writetest();
+    exit();
 }
